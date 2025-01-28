2fdf8fb10f6f1e7f3fc7de9e6366fc6d6aa30bd8
raghavmanish <mnsyd24@gmail.com>
2025-01-28 17:28:06 +0530

    Added some more column in review table with suitable annotations

--- src/main/java/org/example/uberreviewservice/models/Review.java
+++ src/main/java/org/example/uberreviewservice/models/Review.java
@@ -1,10 +1,40 @@
 package org.example.uberreviewservice.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Table(name = "bookingreview")
 public class Review {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id; // Recommended for JPA entities due to its ability to hold null values.
+
+    @Column(nullable = false)
+    String content;
+
+    @Column(nullable = false)
+    Double rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    Date createdAt;
+
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    Date updatedAt;
 }
